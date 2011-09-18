3d70aed7b91bb762c0665f818bb93f620af41fb5
Mandeep Khera <world.mover@gmail.com>
2011-09-18 10:22:37 +0000

    addded Comp controller with index create destroy

--- app/controllers/comps_controller.rb
+++ app/controllers/comps_controller.rb
@@ -1,5 +1,26 @@
 class CompsController < ApplicationController
+
   def index
+    @user = User.find(params[:user_id])
+    @comp = @user.comps 
+  end
+
+  def create
+    @user = User.find(params[:user_id]) 
+    @comp  = @user.comps.build(params[:comp])
+    if @comp.save
+      flash[:success] = "Competition created!"
+      render home_path
+    else
+      render home_path
+    end
+  end
+
+  def destroy
+    @comp = Comp.find(params[:id])
+    Comp.find(params[:id]).destroy
+    flash[:success] = "Competition deleted"
+    render home_path
   end
 
 end
