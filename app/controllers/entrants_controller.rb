e40de55f09810711aa5c5869310189418fc9beab
Mandeep Khera <world.mover@gmail.com>
2011-10-05 20:55:25 +0000

    adding skeleton for entrants controller

---
+++ app/controllers/entrants_controller.rb
@@ -0,0 +1,31 @@
+class EntrantsController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def show
+ 
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+end
